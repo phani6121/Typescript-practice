8ae9d6b3828c9950afbfbe35cc5bc6f9df140da3
PHANINDRA KUMAR NIDAMANURI <git config --global user.email>
2023-11-05 20:53:44 +0530

    numbermethods-objects

--- data-type/number/methods/numbermethods.js
+++ data-type/number/methods/numbermethods.js
@@ -1,2 +1,7 @@
-var infinitevalue = 1 / 0;
-console.log("infinite", Number.isFinite(infinitevalue));
+// number methods 
+// toFixed: it is defind as a number is fixed at the given number in method 
+var num = "200.34567";
+console.log(num + 20); // 20020
+console.log(Number.parseInt(num) + 20); // 220
+console.log(Number.parseFloat(num) + 20); //220.34567
+console.log(Number.parseFloat((Number.parseFloat(num)).toFixed(2)) + 20); //220.34
--- data-type/number/methods/numbermethods.ts
+++ data-type/number/methods/numbermethods.ts
@@ -1,6 +1,26 @@
+// number methods 
+
+let num: string = "200.34567";
+
+console.log(num + 20); // 20020
+
+//In this function output given to string 
+
+console.log(Number.parseInt(num) + 20); // 220
+
+//In this function only taken only before point number only consider 
+
+console.log(Number.parseFloat(num) + 20); //220.34567
+
+//In this function also taken after points also consider 
+
+const toFixedValue: string = Number.parseFloat(num).toFixed(2);
+
+//In this function given out put given in string after neet to change to number 
+
+/*In this function given to some fixed number 
+given to consider after point only out put given only taken that two numbers */
 
 
+console.log(Number.parseFloat(toFixedValue) + 20); //220.34
 
-
-let infinitevalue: number = 1 / 0;
-console.log("infinite", Number.isFinite(infinitevalue))
