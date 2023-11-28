1cba504f5f63d89f2f8277574ac6b9db5b867337
PHANINDRA KUMAR NIDAMANURI <git config --global user.email>
2023-11-28 21:24:02 +0530

    ES6 modifications

--- data-type/enums/enums.js
+++ data-type/enums/enums.js
@@ -8,30 +8,27 @@ var fruitOne = fruitPrice.applePrice;
 var fruittwo = fruitPrice.bananaPrice;
 console.log(fruitOne);
 console.log(fruittwo);
-var fruitPrice1;
-(function (fruitPrice1) {
-    fruitPrice1[fruitPrice1["applePrice1"] = 2] = "applePrice1";
-    fruitPrice1[fruitPrice1["bananaPrice2"] = 3] = "bananaPrice2";
-})(fruitPrice1 || (fruitPrice1 = {}));
-var fruitOne1 = fruitPrice1.applePrice1;
-var fruittwo2 = fruitPrice1.bananaPrice2;
-console.log(fruitOne1);
-console.log(fruittwo2);
-var seat;
-(function (seat) {
-    seat["window"] = "window";
-    seat["middle"] = "middle";
-    seat["cornar"] = "cornar";
-})(seat || (seat = {}));
-var window1 = seat.window;
-var middle1 = seat.middle;
-var cornar1 = seat.cornar;
-console.log(window1);
-console.log(middle1);
-console.log(cornar1);
-var num = 12;
-var obj = {
-    num: num,
-    p: 3
-};
-console.log(obj);
+// enum fruitPrice1 {
+//     applePrice1 = 2, //Here item one price given 2 in this type of case its taken sequence so iten two 3
+//     bananaPrice2
+// }
+// const fruitOne1 = fruitPrice1.applePrice1
+// const fruittwo2 = fruitPrice1.bananaPrice2
+// console.log(fruitOne1);
+// console.log(fruittwo2);
+// enum seat {
+//     window = "window", // Here i am given string so its constant 
+//     middle = "middle",
+//     cornar = "cornar"
+// }
+// const window1 = seat.window
+// const middle1 = seat.middle
+// const cornar1 = seat.cornar
+// console.log(window1);
+// console.log(middle1);
+// console.log(cornar1);
+var num;
+(function (num) {
+    num[num["num"] = 0] = "num";
+    num[num["nums2"] = 1] = "nums2";
+})(num || (num = {}));
---
+++ es6JavaScript/index.js
@@ -0,0 +1,132 @@
+// //JavaScript ES6 is the newer version of JavaScript that was introduced in 2015
+// //let and const
+// function numberOfBikes() {
+//     for (var i = 0; i < 6; i++) {// var key word accesable inside of the function.
+//         console.log(i);
+//     };
+//     console.log(i);
+// };
+// numberOfBikes();
+// function numberBikes() {
+//     for (let i = 0; i < 6; i++) {// let key word accesable inside the function block.
+//         console.log(i); // const key word also accessable inside the function block.
+//     };
+// };
+// numberOfBikes();
+// //Objects 
+// const person = {
+//     name: "srinu",
+//     walk: function () { },// walk is method of the person object.
+//     talk() { }
+// };
+// person.name = "hari"; // simple way of change the values of keys. this is dot notation.
+// person["name"] = "hari"// another way
+// console.log(person);
+// //This key word
+// // this key word is used to find the easy way of the find the values at many objects.
+// // this means the object like laptop1 and laptop2
+// let laptop1 = {
+//     cpu: "i7",
+//     ram: 16,
+//     brand: "HP",
+//     discreption: function () {
+//         console.log(this.cpu);
+//     }
+// };
+// let laptop2 = {
+//     cpu: "i9",
+//     ram: 8,
+//     brand: "Apple",
+//     discreption: function () {
+//         console.log(this.cpu);
+//     }
+// };
+// laptop2.discreption();
+// //Arrow functions
+// //In function lines of code is more thats why arrow function is simplified the function.
+// //In arrow functions return is default in function.
+// const greetings = () => console.log("good morning");
+// greetings();// this is calling in functions  
+// //this arrow function is in function 5 lines of code at here 3 lines 
+// const test = (number1, number2) => number1 + number2
+// let sum = test(20, 50)
+// console.log(sum);
+// //destructring 
+// //It means in one object keys are destructrings to equal to object
+// const person1 = {
+//     name1: "phani",
+//     age: 26,
+//     gender: "male"
+// };
+// const { name1 , age, gender } = person1;
+// console.log(name);
+// //spread 
+// //In this operation used to add the elemets of the one array to another array same like objects also
+// const arrar1=[14,15,16];
+// const array2=[...arrar1,17,18]
+// console.log(array2);
+// const obj1={
+//     name:"siva"
+// };
+// const obj2={
+//     ...obj1,
+//     age:23,
+//     mail:"@gmail.com"
+// };
+// console.log(obj2);
+// function sumarray(...theArgs:any):number { //"...theArgs "= [1,2,3];
+//     let total = 0;
+//     console.log(theArgs);
+//     for (const arg of theArgs) {
+//         total += arg;//total=total+arg
+//     }
+//     return total;
+// }
+// console.log(sumarray(1, 2, 3));
+// // Expected output: 6
+// console.log(sumarray(1, 2, 3, 4));
+// //classes is the buleprint of number of objects
+var greetings1 = /** @class */ (function () {
+    function greetings1() {
+        console.log("good morning");
+    }
+    return greetings1;
+}());
+;
+new greetings1(); // this is the object 
+var employess = /** @class */ (function () {
+    function employess(id, emane, depNo) {
+        this.id = id;
+        this.emane = emane;
+        this.depNo = depNo;
+    }
+    //initilize the veriable using method
+    // setData(id: number, emane: string, depNo: number): void {
+    //     this.id = id;
+    //     this.emane = emane;
+    //     this.depNo = depNo
+    // }
+    //initilize the variable using object veriable
+    employess.prototype.display = function () {
+        console.log(this.id);
+        console.log(this.emane);
+        console.log(this.depNo);
+    };
+    return employess;
+}());
+// let emp = new employess()// this is the object and like this object create number of objects like that
+// emp.id = 1;
+// emp.emane = "srinu";
+// emp.depNo = 1;
+// emp.setData(1, "srinu", 1)
+// emp.display();
+// // let emp2 = new employess()
+// // emp2.id = 2;
+// // emp2.emane = "hari";
+// // emp2.depNo = 1;
+// emp2.setData(2, "hari", 1)
+// emp2.display();
+var emp = new employess(1, "srinu", 1);
+emp.display();
+var emp2 = new employess(2, "hari", 1);
+emp2.display();
